7b0640bb68adf203f61eee27a638bf03eda55738
CodeAndGin <diarmaidbrennan@outlook.com>
2020-12-14 23:40:15 +0000

    Day8 Complete

--- Day8/day8.py
+++ Day8/day8.py
@@ -3,3 +3,48 @@ import sys
 with open(str(sys.argv[1])) as ifile:
     inputs = ifile.readlines()
 
+
+def boot(inputs):
+    accumulator = 0
+    instructionIter = 0
+    instructionsCompleted =[]
+    while True:
+        if instructionIter in instructionsCompleted:
+            return "loop"
+        if instructionIter >= len(inputs):
+            return accumulator
+
+        if inputs[instructionIter].find("nop") == 0:
+            instructionsCompleted.append(instructionIter)
+            instructionIter += 1
+            continue
+
+        if inputs[instructionIter].find("jmp") == 0:
+            instructionsCompleted.append(instructionIter)
+            instructionIter += int(inputs[instructionIter][4:])
+            continue
+
+        if inputs[instructionIter].find("acc") == 0:
+            instructionsCompleted.append(instructionIter)
+            accumulator += int(inputs[instructionIter][4:])
+            instructionIter += 1
+            continue
+
+
+for i,line in enumerate(inputs):
+    copy = inputs.copy()
+    if line.find("nop") == 0:
+        copy[i] = line.replace("nop", "jmp")
+        if boot(copy) == "loop":
+            copy[i] = line
+            continue
+        print(boot(copy))
+        break
+    if line.find("jmp") == 0:
+        copy[i] = line.replace("jmp", "nop")
+        if boot(copy) == "loop":
+            copy[i] == line
+            continue
+        print(boot(copy))
+        break
+
